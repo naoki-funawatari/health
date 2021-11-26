a35e6191f587ae013fead21d3401fb1f4e3ac7a5
naoki-funawatari <g011c1180@gmail.com>
2021-11-26 11:36:14 +0900

    feat: 選択地の初期を設定。

--- src/features/list/CheckList.jsx
+++ src/features/list/CheckList.jsx
@@ -1,25 +1,37 @@
-import { useTaegetDate } from "@/hooks/hooks";
+import { useTaegetDate, useFetchHealthData } from "@/hooks/hooks";
 import employees from "@/db/master/employees.json";
 import conditions from "@/db/master/conditions.json";
 
 const CheckList = ({ syncScroll }) => {
-  const { days } = useTaegetDate();
+  const { year, month, days } = useTaegetDate();
+  const healthData = useFetchHealthData(year, month);
 
   return (
     <div className="check-list-wrapper" onScroll={e => syncScroll(e)}>
-      {employees.map(o => (
-        <div className="flex-row" key={`check-list-${o.no}`}>
-          {days.map((o, i) => (
-            <div className="grid-item" key={`check-list-${o.no}-${i}`}>
-              <select>
-                {conditions.map((o, p) => (
-                  <option key={`check-list-${o.no}-${p}`}>{o.name}</option>
-                ))}
-              </select>
-            </div>
-          ))}
-        </div>
-      ))}
+      {employees.map(employee => {
+        const personalData = healthData.filter(o => o.employee_id === employee.id);
+
+        return (
+          <div className="flex-row" key={`check-list-${employee.no}`}>
+            {days.map((day, i) => {
+              const dayData = personalData.find(o => o.date === day.format("YYYY/MM/DD 00:00:00"));
+              console.log(dayData);
+
+              return (
+                <div className="grid-item" key={`check-list-${day.no}-${i}`}>
+                  <select defaultValue={dayData?.condition_id ?? 0}>
+                    {conditions.map((o, p) => (
+                      <option key={`check-list-${o.no}-${p}`} value={o.id}>
+                        {o.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,6 +1,8 @@
+import { useMemo } from "react";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import reports from "@/db/records/reports.json";
 
 const useTaegetDate = () => {
   dayjs.extend(utc);
@@ -10,11 +12,24 @@ const useTaegetDate = () => {
   const tzDate = dayjs().tz();
   const tzStartDate = tzDate.startOf("month");
   const tzEndtDate = tzDate.endOf("month");
+  const year = Number(tzStartDate.format("YYYY"));
   const month = Number(tzStartDate.format("M"));
   const endDay = Number(tzEndtDate.format("D"));
   const days = [...new Array(endDay).keys()].map((_, i) => tzStartDate.add(i, "day").clone());
 
-  return { month, days };
+  return { year, month, days };
 };
 
-export { useTaegetDate };
+const useFetchHealthData = (year, month) => {
+  return useMemo(() => {
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+    dayjs.tz.setDefault("Asia/Tokyo");
+
+    return reports
+      .filter(o => Number(dayjs(o.date).tz().format("YYYY")) === Number(year))
+      .filter(o => Number(dayjs(o.date).tz().format("M")) === Number(month));
+  }, [year, month]);
+};
+
+export { useTaegetDate, useFetchHealthData };
