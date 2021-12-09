c4321b9a089dd0389e96c13d8811d5fe77a550f0
naoki-funawatari <g011c1180@gmail.com>
2021-12-09 13:37:53 +0900

    feat: モーダルを表示。

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Modal from "react-modal";
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import Main from "@/components/Main";
+
+Modal.setAppElement("#root");
 
 const App = () => (
   <>
--- src/features/checklist/Grid.tsx
+++ src/features/checklist/Grid.tsx
@@ -1,5 +1,19 @@
-import { useConditions, useEmployees, useFetchHealthData } from "@/hooks/hooks";
+import { useState } from "react";
+import ReactModal, { Styles } from "react-modal";
 import { Dayjs } from "dayjs";
+import { useConditions, useEmployees, useFetchHealthData } from "@/hooks/hooks";
+
+const modalStyle: Styles = {
+  overlay: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    position: "static",
+    inset: "auto",
+  },
+};
 
 const Grid = ({
   syncScroll,
@@ -12,9 +26,17 @@ const Grid = ({
   month: string;
   days: Dayjs[];
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const healthData = useFetchHealthData(year, month);
   const conditions = useConditions();
   const employees = useEmployees();
+  const handleDialogOpen = (e: React.ChangeEvent) => {
+    const element = e.target as HTMLSelectElement;
+    if ([4, 5].includes(Number(element.value))) {
+      setIsOpen(true);
+    }
+  };
+  const handleDialogClose = () => setIsOpen(false);
 
   if (healthData.isLoading || employees.isLoading || conditions.isLoading) {
     console.log("isLoading");
@@ -27,31 +49,49 @@ const Grid = ({
     console.log("isFetching");
 
   return (
-    <div className="check-list-wrapper" onScroll={e => syncScroll(e.nativeEvent)}>
-      {employees.data?.map(employee => {
-        const personalData = healthData.data?.filter(o => o.employee_id === employee.id);
+    <>
+      <div className="check-list-wrapper" onScroll={e => syncScroll(e.nativeEvent)}>
+        {employees.data?.map(employee => {
+          if (!healthData) {
+            return <></>;
+          }
+          const personalData = healthData.data?.filter(o => o.employee_id === employee.id);
 
-        return (
-          <div className="flex-row" key={`check-list-${employee.no}`}>
-            {days.map((day, i) => {
-              const dayData = personalData?.find(o => o.date === day.format("YYYY/MM/DD"));
+          return (
+            <div className="flex-row" key={`check-list-${employee.no}`}>
+              {days.map((day, i) => {
+                const dayData = personalData?.find(o => o.date === day.format("YYYY/MM/DD"));
 
-              return (
-                <div className="grid-item" key={`check-list-${employee.no}-${i}`}>
-                  <select defaultValue={dayData?.condition_id ?? 0}>
-                    {conditions.data?.map((o, p) => (
-                      <option key={`check-list-${employee.no}-${o.id}`} value={o.id}>
-                        {o.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+                return (
+                  <div className="grid-item" key={`check-list-${employee.no}-${i}`}>
+                    <select defaultValue={dayData?.condition_id} onChange={handleDialogOpen}>
+                      {conditions.data?.map(condition => (
+                        <option
+                          key={`check-list-${employee.no}-${condition.id}`}
+                          value={condition.id}
+                        >
+                          {condition.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <ReactModal {...{ isOpen }} contentLabel="Settings" style={modalStyle}>
+        <h2>ここに理由を入力</h2>
+        <div>
+          <input type="text" name="" id="" />
+        </div>
+        <br />
+        <div>
+          <button onClick={handleDialogClose}>ダイアログを閉じる</button>
+        </div>
+      </ReactModal>
+    </>
   );
 };
 
