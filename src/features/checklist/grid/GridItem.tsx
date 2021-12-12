eaec6ccb22422c4e7a48198ca4f9c74f1a671fef
naoki-funawatari <g011c1180@gmail.com>
2021-12-12 14:25:51 +0900

    refactor: 関数名を修正。

--- src/features/checklist/grid/GridItem.tsx
+++ src/features/checklist/grid/GridItem.tsx
@@ -11,7 +11,7 @@ const GridItem = (props: IGridItem) => {
   const { employee, report } = props;
   const setIsOpen = useSetRecoilState<boolean>(isOpenState);
   const conditions = useRecoilValue(conditionsState);
-  const handleDialogOpen = (e: React.ChangeEvent) => {
+  const handleReasonChanged = (e: React.ChangeEvent) => {
     const element = e.target as HTMLSelectElement;
     if ([4, 5].includes(Number(element.value))) {
       setIsOpen(true);
@@ -20,7 +20,7 @@ const GridItem = (props: IGridItem) => {
 
   return (
     <div className="grid-item">
-      <select defaultValue={report.condition_id} onChange={handleDialogOpen}>
+      <select defaultValue={report.condition_id} onChange={handleReasonChanged}>
         {conditions.map(condition => (
           <option key={`check-list-${employee.no}-${condition.id}`} value={condition.id}>
             {condition.name}
