dca4623845cb99605e0f4259b8517a1b1440bbdd
naoki-funawatari <g011c1180@gmail.com>
2021-12-15 20:34:12 +0900

    refactor: hooks 関連を手直し。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,13 @@
 import { memo } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { reportsState, changedReportsState } from "@/stores/stores";
-import { updateReports } from "@/apis/apis";
-import { useTaegetDate } from "@/hooks/hooks";
+import { useRecoilValue } from "recoil";
+import { changedReportsState } from "@/stores/stores";
+import { useTaegetDate, useUpdateReports } from "@/hooks/hooks";
 
 const Header = memo(() => {
   const { year, month } = useTaegetDate();
   const monthlyReports = useRecoilValue(changedReportsState);
-  const setReports = useSetRecoilState(reportsState);
-  const handlePushAndPullClicked = async () => {
-    const newReports = await updateReports(year, month, monthlyReports);
-    setReports(newReports);
-  };
+  const { mutateAsync } = useUpdateReports(year, month, monthlyReports);
+  const handlePushAndPullClicked = async () => await mutateAsync();
 
   return (
     <header>
